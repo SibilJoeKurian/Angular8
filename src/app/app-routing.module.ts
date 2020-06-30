b957761e1c47c02f96ca8743d2dccb5680474607
SibilJoeKurian <Sibil.kurian@gmail.com>
2020-06-30 12:08:15 -0400

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PricingTablesComponent } from './pricing-tables/pricing-tables.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ClientsComponent } from './clients/clients.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about',component:IntroductionComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'pricing',component:PricingTablesComponent},
+  {path:'testimonials',component:TestimonialsComponent},
+  {path:'clients',component:ClientsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentsComponent } from './contents/contents.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingTablesComponent } from './pricing-tables/pricing-tables.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    FooterComponent,
+    ContentsComponent,
+    GalleryComponent,
+    TestimonialsComponent,
+    ClientsComponent,
+    PricingTablesComponent,
+    NavigationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
